1f7ac1da46c303d8800dc4c4400c766362627dd7
Arjav Jain <arjavjain0703@gmail.com>
2026-06-30 20:10:30 +0530

    fetch status from backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  let status = fetch('/api/health')
+    .then((res) => res.json())
+    .then((data) => data.status);
+</script>
+
+<h1> Hi </h1>
+<p>Server status: {#await status then s}{s}{/await}</p>
